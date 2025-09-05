8c590d699726363578e6fcd6626ccb61ee5ebe24
Gavin Chin <1640786+slowmonkey@users.noreply.github.com>
2025-09-05 16:26:10 +0800

    Adding initial merge sort files

---
+++ LearningMergeSort.Tests/MergeSortTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MergeSort.Tests;
+
+public class MergeSortTests
+{
+    [Theory]
+    [InlineData(new int[] { 7, 1, 9, 2, 5 }, new int[] { 1, 2, 5, 7, 9 })]
+    [InlineData(new int[] { 42, 7, 19, 3, 88, 54, 23, 16, 71, 5, 30, 12, 91, 2, 67, 49, 81, 14, 60, 35 }, new int[] {2, 3, 5, 7, 12, 14, 16, 19, 23, 30, 35, 42, 49, 54, 60, 67, 71, 81, 88, 91})]
+    public void ValidInput_SortedResult(int[] unsortedArray, int[] expectedResult)
+    {
+        LearningMergeSort.MergeSort(unsortedArray);
+
+        Assert.Equal(expectedResult, unsortedArray);
+    }
+}
---
+++ LearningMergeSort/LearningMergeSort.cs
@@ -0,0 +1,78 @@
+﻿namespace MergeSort;
+
+public class LearningMergeSort
+{
+    public static void MergeSort(int[] items)
+    {
+        // Stop processing if the number of items in the array is 1 or less
+
+        if (items.Length <= 1)
+        {
+            return;
+        }
+
+        // Split the array into 2.
+
+        int middleIndex = items.Length / 2;
+
+        int[] left = new int[middleIndex];
+        int[] right = new int[items.Length - middleIndex];
+
+        Array.Copy(items, 0, left, 0, middleIndex);
+        Array.Copy(items, middleIndex, right, 0, items.Length - middleIndex);
+
+        // Sort each array
+
+        MergeSort(left);
+        MergeSort(right);
+
+        // Merge the sorted halves
+
+        Merge(items, left, right);
+    }
+
+    private static void Merge(int[] result, int[] left, int[] right)
+    {
+        int resultIndex = 0;
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        // Loop through the maximum length array number of times.
+
+        while (resultIndex < result.Length)
+        {
+            if (leftIndex < left.Length && rightIndex < right.Length)
+            {
+                if (left[leftIndex] < right[rightIndex])
+                {
+                    result[resultIndex] = left[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    result[resultIndex] = right[rightIndex];
+
+                    rightIndex++;
+                }
+            }
+            else
+            {
+                // Assumes one of the left or right indexs are already merged
+                
+                if (leftIndex < left.Length)
+                {
+                    result[resultIndex] = left[leftIndex];
+                    leftIndex++;
+                }
+
+                if (rightIndex < right.Length)
+                {
+                    result[resultIndex] = right[rightIndex];
+                    rightIndex++;
+                }
+            }
+
+            resultIndex++;
+        }
+    }
+}
